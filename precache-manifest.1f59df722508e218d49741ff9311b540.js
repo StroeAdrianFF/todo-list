b5c00459fecfe3fdeef070a1ba895933e3c0c275
Adrian <contact@stroe-adrian.ro>
2020-09-06 13:11:40 +0300

    Updates

---
+++ precache-manifest.1f59df722508e218d49741ff9311b540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3cce7576d8d83000ca5da9b685e88f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f877f5938547705e50c6",
+    "url": "/todo-list/static/css/main.185f231c.chunk.css"
+  },
+  {
+    "revision": "b7b3c787e0542a80a5f8",
+    "url": "/todo-list/static/js/2.ab8f88eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.ab8f88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f877f5938547705e50c6",
+    "url": "/todo-list/static/js/main.36ad8edb.chunk.js"
+  },
+  {
+    "revision": "7b15bc2f73099ab4ac07",
+    "url": "/todo-list/static/js/runtime-main.21a12556.js"
+  }
+]);
